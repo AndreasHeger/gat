169e7f20456d448e616be928e138cd5003e9d33d
andreas.heger@gmail.com <andreas.heger@gmail.com>
2013-03-01 02:54:29 +0000

    first refactoring of package for install
    --HG--
    rename : engine/cgat.pxd => GatEngine/engine/cgat.pxd
    rename : engine/cgat.pyx => GatEngine/engine/cgat.pyx
    rename : segmentlist/csegmentlist.pxd => GatSegmentList/segmentlist/csegmentlist.pxd
    rename : segmentlist/csegmentlist.pyx => GatSegmentList/segmentlist/csegmentlist.pyx

--- setup.py
+++ setup.py
@@ -12,21 +12,24 @@ import os
 import sys
 import glob
 
-#from ez_setup import use_setuptools
-#use_setuptools()
+from distribute_setup import use_setuptools
+use_setuptools()
 
-#from setuptools import Extension, setup
+from setuptools import Extension, setup, find_packages
 
-from distutils.core import setup, Extension
-from Cython.Distutils import build_ext
-
-## note that for automatic cythoning, 
-## both pyrex and cython need to be installed.
-## see http://mail.python.org/pipermail/distutils-sig/2007-September/008204.html
-#try:
-#    from Cython.Distutils import build_ext
-#except:
-#    from setuptools.command.build_ext import build_ext
+#######################################################
+#######################################################
+try:
+    from Cython.Distutils import build_ext
+except ImportError:
+    # no Cython available - use existing C code
+    cmdclass = { }
+    GatSegmentList_sources = ['GatSegmentList/csegmentlist.c', 'utils/gat_utils.c']
+    GatEngine_sources = ['GatEngine/cgat.c', 'utils/gat_utils.c' ]
+else:
+    cmdclass = { 'build_ext' : build_ext }
+    GatSegmentList_sources = ['GatSegmentList/csegmentlist.pyx', 'utils/gat_utils.c']
+    GatEngine_sources = ['GatEngine/cgat.pyx', 'utils/gat_utils.c' ]
 
 name = "gat"
 version = "0.1"
@@ -45,22 +48,25 @@ Topic :: Scientific/Engineering :: Bioinformatics
 """
 
 # link against rt for shared memory access
-segmentlist = Extension(
-    "csegmentlist",                   # name of extension
-    [ "segmentlist/csegmentlist.pyx", "utils/gat_utils.c" ],
-    libraries=[ "z", 'rt' ],
-    library_dirs = [],
-    include_dirs=['./utils', "/usr/lib64/python2.6/site-packages/numpy/core/include", '.'],
-    language="c",
-    )
+GatSegmentList = Extension(
+   "csegmentlist",                   # name of extension
+   GatSegmentList_sources,
+   libraries=[ "z", 'rt' ],
+   library_dirs = [],
+   include_dirs=['./utils', 
+                 "/usr/lib64/python2.6/site-packages/numpy/core/include", '.'],
+   language="c",
+   )
 
-cgat = Extension(
+GatEngine = Extension(
     "cgat",                   # name of extension
-    [ "engine/cgat.pyx", "utils/gat_utils.c" ],
+    GatEngine_sources,
     libraries=[ "z" ],
     library_dirs = [],
-    include_dirs=["./utils", "/usr/lib64/python2.6/site-packages/numpy/core/include", 
-                  'segmentlist', '.'],
+    include_dirs=["./utils", 
+                  "/usr/lib64/python2.6/site-packages/numpy/core/include", 
+                  'GatSegmentList', 
+                  '.'],
     language="c",
     )
 
@@ -76,14 +82,13 @@ metadata = {
     'url': "http://code.google.com/p/genomic-association-tester/",
     'py_modules': [ x[:-3] for x in glob.glob( 'gat/*.py') ],
     'requires' : ['cython (>=0.12)'],
-    'ext_modules': [ segmentlist, cgat ],
+    'ext_modules': [ GatSegmentList, GatEngine ],
     'cmdclass' : {'build_ext': build_ext },
     'scripts' : ['scripts/gat-run.py', 
                  'scripts/gat-great.py',
                  'scripts/gat-compare.py',
                  'scripts/gat-plot.py' ],
     'zip_safe' :False,
-
    }
 
 if __name__=='__main__':
