237193091a22e30e9529d4743ba0c442a43a9e61
Andreas Heger <andreas.heger@gmail.com>
2014-02-03 17:58:25 +0000

    travis: streamlined installation

--- setup-Python.sh
+++ setup-Python.sh
@@ -1,26 +1,8 @@
 #!/usr/bin/env bash
 
-# Build Python
-cd
-mkdir CGAT
-wget http://www.python.org/ftp/python/2.7.5/Python-2.7.5.tgz
-tar xzvf Python-2.7.5.tgz
-rm Python-2.7.5.tgz
-cd Python-2.7.5
-./configure --prefix=$HOME/CGAT/Python-2.7.5
-make
-make install
-cd
-rm -rf Python-2.7.5
-
 # Create virtual environment
-cd
-cd CGAT
-wget --no-check-certificate https://pypi.python.org/packages/source/v/virtualenv/virtualenv-1.10.1.tar.gz
-tar xvfz virtualenv-1.10.1.tar.gz
-rm virtualenv-1.10.1.tar.gz
-cd virtualenv-1.10.1
-$HOME/CGAT/Python-2.7.5/bin/python virtualenv.py cgat-venv
+pip install virtualenv
+virtualenv cgat-venv
 source cgat-venv/bin/activate
 
 # Install some Python prerequisites
